454aa9bbba2635a207f10b3c7698e35885f95539
Marta Okilka <martaokilka@gmail.com>
2026-01-20 23:02:22 +0200

    hamburger menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 import "../sass/style.scss";
+
+const hamburger = document.querySelector(".hamburger"),
+	menu = document.querySelector(".menu"),
+	closeElem = document.querySelector(".menu__close");
+
+hamburger.addEventListener("click", () => {
+	menu.classList.add("active");
+});
+
+closeElem.addEventListener("click", () => {
+	menu.classList.remove("active");
+});
