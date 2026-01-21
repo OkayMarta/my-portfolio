9ce65308d3f1e97913d4a9ab1fd8018bc9e436b8
Marta Okilka <martaokilka@gmail.com>
2026-01-21 12:54:59 +0200

    sidepanel color change

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,24 @@ hamburger.addEventListener("click", () => {
 closeElem.addEventListener("click", () => {
 	menu.classList.remove("active");
 });
+
+const sidepanel = document.querySelector(".sidepanel");
+const promo = document.querySelector(".promo");
+
+// Функція перевірки скролу
+function checkScroll() {
+	// Отримуємо висоту першої секції
+	const promoHeight = promo.offsetHeight;
+
+	if (window.scrollY > promoHeight - 200) {
+		sidepanel.classList.add("sidepanel_black");
+	} else {
+		sidepanel.classList.remove("sidepanel_black");
+	}
+}
+
+// Слухаємо подію скролу
+window.addEventListener("scroll", checkScroll);
+
+// Викликаємо один раз при завантаженні (на випадок, якщо сторінку оновили посередині)
+checkScroll();
